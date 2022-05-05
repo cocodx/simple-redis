0c4d7f112d43f7d1113ec70820bdbf38e729e9d6
刘刚 <2cevening@gmail.com>
2022-05-06 00:03:10 +0800

    springboot连接cluster集群
    在yml文件里面，换了一下cluster，nodes，password，注释掉sentinel

--- springboot-sentinel/src/main/java/com/rediscode/use/controller/IndexController.java
+++ springboot-sentinel/src/main/java/com/rediscode/use/controller/IndexController.java
@@ -29,4 +29,19 @@ public class IndexController {
             }
         }
     }
+
+    @RequestMapping("/testCluster")
+    public void testCluster(){
+        int i=1;
+        while (true){
+            try{
+                stringRedisTemplate.opsForValue().set("zhuge"+i,i+"");
+                System.out.println("设置key："+"zhuge"+i);
+                i++;
+                Thread.sleep(1000);
+            }catch (Exception e){
+                logger.error("error:",e);
+            }
+        }
+    }
 }
