d0592fad7aebee4d833fd482eb9129085db09a8d
刘刚 <2cevening@gmail.com>
2022-05-27 06:09:17 +0800

    fixed-update(jedis-use):使用lua脚本，setnx+px，保证原子性

--- jedis-use/src/main/java/com/rediscode/use/lock/RedisTest1.java
+++ jedis-use/src/main/java/com/rediscode/use/lock/RedisTest1.java
@@ -2,6 +2,8 @@ package com.rediscode.use.lock;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
+
 /**
  * 设置setnx分布式锁，设置过期时间,缺乏原子性，
  * 如果在setnx和expire之间出现异常，锁无法释放
@@ -22,5 +24,21 @@ public class RedisTest1 {
         long result = jedis.setnx(lockKey,"1");
         jedis.expire(lockKey,10);
         return result>0;
+    }
+
+    /**
+     * setnx + px放在一起 lua脚本，保证原子性
+     * @param jedis
+     */
+    public static void testLua(Jedis jedis){
+        String luaStr = "if redis.call('setnx',KEYS[1],ARGV[1]) == 1 then redis.call('expire',KEYS[1],ARGV[2]) return 1 else return 0 end";
+        ArrayList<String> keys = new ArrayList<>();
+        keys.add("nxpxKey");
+        ArrayList<String> args = new ArrayList<>();
+        args.add("nxpxKey-Value");
+        //单位是秒
+        args.add("100000");
+        Object result = jedis.eval(luaStr,keys,args);
+        System.out.println("执行结果lua脚本："+result);
     }
 }
--- jedis-use/src/test/java/com/rediscode/use/LockTest.java
+++ jedis-use/src/test/java/com/rediscode/use/LockTest.java
@@ -2,6 +2,7 @@ package com.rediscode.use;
 
 import com.rediscode.use.lock.JedisUtil;
 import com.rediscode.use.lock.RedisTest;
+import com.rediscode.use.lock.RedisTest1;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
 
@@ -12,5 +13,19 @@ public class LockTest {
         JedisUtil jedisUtil = new JedisUtil();
         Jedis jedis = jedisUtil.getJedis();
         System.out.println("设置setnx成功："+RedisTest.tryLock(jedis,"2022-05-28"));
+    }
+
+    @Test
+    public void testSetNx1(){
+        JedisUtil jedisUtil = new JedisUtil();
+        Jedis jedis = jedisUtil.getJedis();
+        System.out.println("设置setnx成功："+ RedisTest1.tryLock(jedis,"2022-05-28"));
+    }
+
+    @Test
+    public void testSetNx1Lua(){
+        JedisUtil jedisUtil = new JedisUtil();
+        Jedis jedis = jedisUtil.getJedis();
+        RedisTest1.testLua(jedis);
     }
 }
